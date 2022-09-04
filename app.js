1aeccbb3a16bd779c38649e03ba4ac865a42bc23
Nadav Spier <herespier@yahoo.com>
2022-09-04 19:09:44 +0300

    before assignment 3

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
+const path = require('path');
+
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-    console.log('This always runs!'); 
-    next();
-});
+app.use(bodyParser.urlencoded({extended: false})); 
+app.use(express.static(path.join(__dirname, 'public')))
 
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use('/add-product', (req, res, next) => {
-
-    console.log('In another middleware!');
-    res.send('<h1>The "Add Product" page</h1>');
-});
-
-app.use('/', (req, res, next) => {
-
-    console.log('In another middleware!');
-    res.send('<h1>Hello from Express! </h1>');
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 });
 
 app.listen(3000);
